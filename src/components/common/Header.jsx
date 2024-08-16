e630b436c452a3eab1425f5a848382fc728315c6
chy0706-dailynco <chy0706@dailynco.com>
2024-08-16 18:38:45 +0900

    Feat: add header files

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,27 @@
+import headerLeftMenu from "../../../src/assets/icons/header_left_menu.svg";
+import profileIcon from "../../../src/assets/icons/profile_icon.svg";
+import shoppingBag from "../../../src/assets/icons/shopping_bag_icon.svg";
+import "../../styles/common/header.scss";
+
 export default function Header() {
   return (
-    <header>
-      <div></div>
-      <div></div>
-      <div></div>
+    <header className="header">
+      <div className="header__left">
+        <img
+          className="header__left-icon"
+          alt="header-left-icon"
+          src={headerLeftMenu}
+        />
+      </div>
+      <div className="header__logo">Iruly</div>
+      <div className="header__right">
+        <img className="header__profile-icon" alt="profile" src={profileIcon} />
+        <img
+          className="header__shopping-bag"
+          alt="shopping-bag"
+          src={shoppingBag}
+        />
+      </div>
     </header>
   );
 }

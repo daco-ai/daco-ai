9445f7c830f5acd6817f28adc5d9800ee6ff5000
ssb0817 <ssb0817@dailynco.com>
2024-09-06 14:40:09 +0900

    Feat:테스트 페이지 작업완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./pages/main";
 import TestIntro from "./pages/testIntro";
 import "./app.scss";
@@ -6,10 +6,14 @@ import Header from "./components/common/Header";
 import useStore from "./stauts/store";
 import Test from "./pages/test";
 import TestResult from "./pages/testResult";
-import { useEffect } from "react";
+import Data from "./data/testQuestion.json";
+import { useEffect, useReducer } from "react";
 function App() {
-  const currentUrl = window.location.pathname.split("/")[1];
   const { page, setPage } = useStore((state) => state);
+  const location = useLocation();
+
+  const currentUrl = window.location.pathname.split("/")[1];
+
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const level = params.get("level");
@@ -17,6 +21,7 @@ function App() {
       setPage(Number(level)); // URL에서 가져온 level 값으로 page 상태를 설정
     }
   }, [location, setPage]);
+
   return (
     <div className="container">
       {currentUrl !== "test" && <Header />}
@@ -25,7 +30,13 @@ function App() {
         <Route
           path="/test"
           element={
-            page === 0 ? <TestIntro /> : page <= 16 ? <Test /> : <TestResult />
+            page === 0 ? (
+              <TestIntro />
+            ) : page <= Data.length ? (
+              <Test />
+            ) : (
+              <TestResult />
+            )
           }
         />
       </Routes>
